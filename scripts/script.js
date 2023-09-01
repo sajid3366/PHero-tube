1e38a1df062bd803028afdf8ebebf2ef93943856
sajid360 <ahteshamsajid8@gmail.com>
2023-09-01 23:03:59 +0600

    time and varified section added

--- scripts/script.js
+++ scripts/script.js
@@ -18,23 +18,23 @@ const tabContainer = (categorys) => {
 
         `;
         tabContainer.appendChild(div);
-        console.log(category.category_id);
 
-        // console.log(category);
     })
 }
 
 const showCardBtn = async (id) => {
     const cardContainer = document.getElementById('card-container');
     const drawingContainer = document.getElementById('drawing');
+
     cardContainer.textContent = '';
+    drawingContainer.textContent = '';
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
     const data = await res.json();
-    console.log(data);
+    // console.log(data);
     const card = data.data;
-    if(card.length === 0){
+    if (card.length === 0) {
         const div = document.createElement('div');
-        div.innerHTML =`
+        div.innerHTML = `
             <div class="w-[400px] m-auto mt-8">
                 <div class="flex justify-center mb-3"><img src="image/Icon.png" /></div>
                 <p class="text-3xl font-bold text-center">Oops!! Sorry, There is no content here</p>
@@ -44,17 +44,18 @@ const showCardBtn = async (id) => {
         drawingContainer.appendChild(div);
 
     }
+
     card.forEach(element => {
         console.log(element);
         const div = document.createElement('div');
         div.classList = "p-4";
         const postDateInSecText = element.others.posted_date;
-        const vari = document.getElementById('verified');
+
 
         div.innerHTML = `
             <img class="rounded-lg w-[330px] h-[200px]" src="${element.thumbnail}" alt="Shoes" />
-           
-            <p class="bg-black text-white max-w-[200px] absolute px-2 py-1 rounded-lg ml-[130px] mt-[-50px]">${(!!postDateInSecText == true) ? Math.floor(postDateInSecText / 3600) : ''} hrs ${(!!postDateInSecText == true) ? Math.floor((postDateInSecText % 3600) / 60) : ''} min ago</p>
+            <p>${(!!postDateInSecText == true)? `<p class=" bg-black text-white max-w-[200px] absolute px-2 py-1 rounded-lg ml-[130px] mt-[-50px]">${(!!postDateInSecText == true) ? Math.floor(postDateInSecText / 3600) : ''} hrs${(!!postDateInSecText == true) ? Math.floor((postDateInSecText % 3600) / 60) : ''} min ago</p>`: ''}</p>
+            
             <div class="flex gap-3 my-5">
                 <div>
                     <img class="w-10 h-10 rounded-[50%]" src=${element.authors[0]?.profile_picture} />
@@ -65,10 +66,9 @@ const showCardBtn = async (id) => {
                     <div class="flex gap-2 mt-2 mb-2">
                     <p>${element.authors[0]?.profile_name}</p>
                     
-                    <img class="" id="verified" src="image/fi_10629607.png" alt="">
-                    <p></p>
-
-
+                    <p>${(element.authors[0]?.verified == true)? `    <img src="image/fi_10629607.png" alt="">
+                    `:'' }</p>
+                    
                     </div>
                     <p>${element.others?.views} views</p>
                     
@@ -76,9 +76,9 @@ const showCardBtn = async (id) => {
                 </div>
             </div>
     
-    `;
+        `;
         cardContainer.appendChild(div)
-        categoryBtn(id);
+        // categoryBtn(id);
 
 
     })
@@ -86,11 +86,11 @@ const showCardBtn = async (id) => {
 
 }
 
-const categoryBtn = (id) => {
+// const categoryBtn = (id) => {
 
-}
+// }
 
-const blogBtn = ()=>{
+const blogBtn = () => {
     window.location.href = "http://127.0.0.1:5500/blog.html";
 
 }
